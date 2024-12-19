019a94ae40068044f79278c83c62f119641a2858
VictorBaron1 <victorb104@nycstudents.net>
2024-12-19 14:14:18 +0000

    first commit

---
+++ Bag.java
@@ -0,0 +1,21 @@
+public class Bag
+{
+    private Boolean hasBag;
+    private int pencils;
+    private int students;
+    public Bag(Boolean h , int p, int s)
+    {
+       hasBag = h;
+       pencils = p;
+    }
+    public int student()
+    {
+        if(h = true && p >= 3)
+        return true;
+    }
+    public int numberOfStudents()
+    {
+        return s;
+    }    
+}
+ 
